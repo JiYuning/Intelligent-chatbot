f2598febc380e3b91aa2d4dd61c69bb7723175d1
Camaro <jiyuningaini@163.com>
2020-09-29 19:59:17 +0800

    智能回复完成

--- js/chat.js
+++ js/chat.js
@@ -2,10 +2,16 @@ $(function () {
     // 初始化右侧滚动条
     // 这个方法定义在scroll.js中
     resetui()
+    $('#ipt').on('keyup', function (e) {
+        if (e.keyCode == 13) {
+            $('#btnSend').click()
+        }
+    })
     $('#btnSend').on('click', function () {
         var text = $('#ipt').val().trim()
         if (text == '') {
-            alert('请输入要发送的内容！')
+            $('#ipt').val('')
+            return alert('请输入要发送的内容！')
         }
         $('.talk_list').append(
             `
@@ -15,14 +21,26 @@ $(function () {
             `
         )
         $('#ipt').val('')
-
+        getMsg(text)
     })
     function getMsg(text) {
         $.ajax({
             method: 'get',
             url: 'http://www.liulongbin.top:3006/api/robot',
+            data: {
+                spoken: text
+            },
             success: function (res) {
-                console.log(res);
+                var msg = res.data.info.text
+                if (res.message == 'success') {
+                    $('.talk_list').append(
+                        `
+                        <li class="left_word">
+                          <img src="img/person01.png" /> <span>${msg}</span>
+                        </li>
+                        `
+                    )
+                }
             }
 
         })
